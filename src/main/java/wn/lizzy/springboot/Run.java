15b7a7e2413be34a800e6731add1629a3ad7f2d2
nicolastsuei <cuilj1982@126.com>
2017-03-09 16:04:32 +0800

    multi datasource with springboot

---
+++ src/main/java/wn/lizzy/springboot/Run.java
@@ -0,0 +1,19 @@
+package wn.lizzy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主启动程序
+ * Run.java
+ * @author cuilj  2017年3月9日--下午2:57:36
+ *
+ */
+@SpringBootApplication
+public class Run 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(Run.class, args);
+    }
+}
